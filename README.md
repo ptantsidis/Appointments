d3300c3a0a54b07ef7aa64cb174afa7a11125c42
Philip Tantsidis <ptantsidis@gmail.com>
2021-09-27 08:30:50 -0400

    more logic

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Create Appointment Daily Planner
 Jquery to get timer to display in Header
+setup header with current date and time updating every second
+Add Bootstrap formatted list 
+Add source David & Libre from google fonts and made them global
+// creat/name schedule boxes 9 to 5 one for each hour/ box 
+boxes are editable
+times are not editable
+//have past colors greyed out - past box time plus 1 hour - also disables the box 
+// current time equal red 
+future time aequals green
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,79 @@
 let timeDisplayEl = $('#time-display');
-
+let listGroupEl = $('.timeslot');
+let allSaveBtns = document.querySelectorAll("button");
+let timeSlots = document.querySelectorAll("timeslot");
+let i = 0;
+let localStores = [{
+schedTime: "9",
+schedText: "",
+},
+{
+schedTime: "10",
+schedText: "",  
+},
+{
+schedTime: "11",
+schedText: "",
+},
+{
+schedTime: "12",
+schedText: "",  
+},
+{
+schedTime: "13",
+schedText: "",
+},
+{
+schedTime: "14",
+schedText: "",  
+},  
+{
+schedTime: "15",
+schedText: "",
+},
+{
+schedTime: "16",
+schedText: "",  
+},  
+{
+schedTime: "17",
+schedText: "",  
+}]
 function displayTime() {
-    let rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
+    let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(currentTime);
+}
+setInterval(displayTime, 1000);
+
+
+
+for( i = 0; i < allSaveBtns.length; i++){
+  allSaveBtns[i].addEventListener('click', function(){
+   // grab the value from the textarea
+let userTextArea = this.previousSibling.previousSibling.value;
+
+// save to local storage
+
+  })
+}
+let currentHour = moment().format("hh");
+// const timeSlots = document.querySelectorAll("timeslot");
+for( i = 0; i < timeSlots.length; i++) {
+  if (currentHour > timeSlots[i].getAttribute('militaryTime'))
+  $("timeslot").addClass("#past");
+} else if (currentHour === timeSlots[i].getAttribute('militaryTime')) 
+{
+  $("timeslot").addClass("#present");
+} else if (currentHour < timeSlots[i].getAttribute('militaryTime')) 
+{
+  $("timeslot").addClass("#future");
+}
+ 
+  //current
+ 
+    //add the css class to that row
   }
-  setInterval(displayTime, 1000);
+
+}
+
+
